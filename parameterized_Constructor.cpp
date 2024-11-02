cd749579f958484257a29ddfc312310537cd79a3
Varsha Luthra <126249449+varshaluthra246@users.noreply.github.com>
2024-11-02 17:07:10 +0530

    Add files via upload

---
+++ parameterized_Constructor.cpp
@@ -0,0 +1,26 @@
+//C++ Parameterized Constructor
+#include <iostream>  
+using namespace std;  
+class Employee {  
+   public:  
+       int id;//data member (also instance variable)      
+       string name;//data member(also instance variable)  
+       float salary;  
+       Employee(int i, string n, float s)    
+        {    
+            id = i;    
+            name = n;    
+            salary = s;  
+        }    
+       void display()    
+        {    
+            cout<<id<<"  "<<name<<"  "<<salary<<endl;    
+        }    
+};  
+int main(void) {  
+    Employee e1 =Employee(1, "Varsha", 890000); //creating an object of Employee   
+    Employee e2=Employee(2, "Sonam", 59000);   
+    e1.display();    
+    e2.display();    
+    return 0;  
+}  
